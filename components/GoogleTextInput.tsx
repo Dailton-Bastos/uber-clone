7285486cabc4f2bfa366c7bb1b198e70d6c73118
Dailton Bastos <dailtonbastos@gmail.com>
2024-09-22 15:05:20 -0300

    feat: add location autocomplete feature

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, View } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
+import { icons } from '@/constants'
 import type { GoogleInputProps } from '@/@types/types'
+
+const GOOGLE_PLACES_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY
 
 export const GoogleTextInput = ({
 	icon,
@@ -14,7 +18,62 @@ export const GoogleTextInput = ({
 		<View
 			className={`flex flex-row items-center justify-center relative z-50 rounded-xl mb-5 ${containerStyle}`}
 		>
-			<Text>Search</Text>
+			<GooglePlacesAutocomplete
+				fetchDetails
+				placeholder="Where you want to go?"
+				debounce={200}
+				styles={{
+					textInputContainer: {
+						alignItems: 'center',
+						justifyContent: 'center',
+						borderRadius: 20,
+						marginHorizontal: 20,
+						position: 'relative',
+						shadowColor: '#d4d4d4',
+					},
+					textInput: {
+						backgroundColor: textInputBackgroundColor ?? '#fff',
+						fontSize: 16,
+						fontWeight: '600',
+						marginTop: 5,
+						width: '100%',
+						borderRadius: 200,
+					},
+					listView: {
+						backgroundColor: textInputBackgroundColor ?? '#fff',
+						position: 'relative',
+						top: 0,
+						width: '100%',
+						borderRadius: 10,
+						shadowColor: '#d4d4d4',
+						zIndex: 99,
+					},
+				}}
+				onPress={(data, details = null) =>
+					handlePress({
+						latitude: details?.geometry.location.lat!,
+						longitude: details?.geometry.location.lng!,
+						address: data.description,
+					})
+				}
+				query={{
+					key: GOOGLE_PLACES_API_KEY,
+					language: 'en',
+				}}
+				renderLeftButton={() => (
+					<View className="justify-center items-center w-6 h-6">
+						<Image
+							source={icon ? icon : icons.search}
+							className="w-6 h-6"
+							resizeMode="contain"
+						/>
+					</View>
+				)}
+				textInputProps={{
+					placeholderTextColor: 'gray',
+					placeholder: initialLocation ?? 'Where do you want to go?',
+				}}
+			/>
 		</View>
 	)
 }
